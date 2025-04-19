28b308919038d300c1e7cb24a15d1d29828e526c
مُحَمَّد سَعْدَاوِيْ <m3md69@outlook.com>
2025-04-20 00:53:31 +0200

    # Edit NestedForLoops3.cpp File

--- src/_38_nested_for_loops/NestedForLoops3.cpp
+++ src/_38_nested_for_loops/NestedForLoops3.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     cout << "Enter the starting number:" << endl;
-    int startingNumber;
-    cin >> startingNumber;
+    int endingNumber;
+    cin >> endingNumber;
 
-    for (int firstIndex = 0; firstIndex <= startingNumber; ++firstIndex) {
-        for (int secondIndex = 1; secondIndex <= startingNumber - firstIndex; ++secondIndex)
-            cout << secondIndex << " ";
+    for (int round = 1; round <= endingNumber; ++round) {
+        for (int index = round; index <= endingNumber; ++index)
+            cout << index << " ";
         cout << endl;
     }
 }
